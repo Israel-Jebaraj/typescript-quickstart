f1e11f263a6788514b330dca575e9d3476067c3a
Israel Jebaraj <93191692+Israel-Jebaraj@users.noreply.github.com>
2022-01-07 20:13:20 +0530

    login.ts file added

---
+++ src/login.ts
@@ -0,0 +1,8 @@
+export class login{
+    constructor(){}
+    public showRegistrationPage(){
+        $("#login-heading").html("Registration");
+        $("#container .login-form").hide();
+
+    }
+}
