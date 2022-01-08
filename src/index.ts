c7c7841ff738326603b43005eaca226a2ad3ca41
Israel Jebaraj <israeljebaraj.c@syncfusion.com>
2022-01-08 13:16:04 +0530

    login page - commit changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
-import {login} from './login.js';
+import {login,user,userLogin} from './login.js';
+let users:user[]=[];
+let user1:user={
+    name:"sample",
+    age:21,
+    password:"sample"
+}
+users.push(user1);
 $(document).ready(function(){
-    $(".link").on('click',function(){
-       new login().showRegistrationPage();
+    $(".link").on("click",function(){
+        new login().showRegistrationPage();      
+    });
+    $("#container .login-form #loginBtn").on("click",function(){
+        new login().ValidateLogin(users);      
     });
 });
+
