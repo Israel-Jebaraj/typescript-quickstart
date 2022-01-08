fa2255a887f3b376977aafba4c7c1219d8f5337e
Israel Jebaraj <israeljebaraj.c@syncfusion.com>
2022-01-08 17:11:21 +0530

    Registration commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
-import {login,user,userLogin} from './login.js';
+import {login,registeredUser,user,userLogin} from './login.js';
 let users:user[]=[];
 let user1:user={
     name:"sample",
@@ -8,11 +8,24 @@ let user1:user={
 }
 users.push(user1);
 $(document).ready(function(){
-    $(".link").on("click",function(){
+    loginRelated();
+});
+
+function loginRelated()
+{
+    $("#container .register-form").hide();
+    $(".register-link").on("click",function(){
         new login().showRegistrationPage();      
     });
+    $(".login-link").on('click',function(){
+        $("#login-heading").html("Login");
+        $("#container .register-form").hide();
+        $("#container .login-form").show();
+    })
     $("#container .login-form #loginBtn").on("click",function(){
         new login().ValidateLogin(users);      
     });
-});
-
+    $("#container .register-form #registerBtn").on("click",function(){
+        let registeredUserdetail:registeredUser=new login().validateRegistration();     
+    });
+}
--- src/login.ts
+++ src/login.ts
@@ -12,7 +12,7 @@ export  class login{
             let password:string=$(".login-form #pass").val();
             let reqUser:user=users.filter((u:user)=>u.name===name&&u.password===password)[0];
             if(reqUser){
-                $("#container .login-form").find('input').removeClass('error');
+                $("#container .login-form").find('input').removeClass('error');                
                 alert("done");
             }
             else{
@@ -23,9 +23,34 @@ export  class login{
             alert("Not a valid user:")
         }
     }
-     showRegistrationPage():void{
+    public validateRegistration():registeredUser{
+        let result:registeredUser={
+            validRegistration:false,
+            userDetails: new user("",0,"")
+        };
+        let id:string='#container .register-form';
+        let name:string=$(id).find('input#name').val();
+        let age:string=$(id).find('input#age').val();
+        let pass:string=$(id).find('input#pass').val();
+        let cpass:string=$(id).find('input#cpass').val();
+        if(pass!==cpass){
+           alert('password not match')
+        }
+        else if(name !== undefined && name !== null && name.length>0 && /^[0-9][0-9]$/.test(age)){
+            result={
+                validRegistration:true,
+                userDetails:new user(name,parseInt(age),pass)
+            }
+            alert("passed")
+        }
+        return result;
+
+    }
+   
+     public showRegistrationPage():void{
         $("#login-heading").html("Registration");
-        $("#container .login-form").hide();    
+        $("#container .login-form").hide();
+        $("#container .register-form").show();
         
     }
     add(a:number,b:number):number{
@@ -33,6 +58,10 @@ export  class login{
     }
     
     
+}
+export interface registeredUser{
+    validRegistration:boolean;
+    userDetails:user;
 }
 export interface userLogin{
     name:string,
