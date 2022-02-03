b371cf102f4192c2dc58dcf5053533874b2105b5
Israel Jebaraj <israeljebaraj.c@syncfusion.com>
2022-02-03 18:55:55 +0530

    ToDo Logic

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
-$("#container").html("This is done jquery intelligence");
+let id:number=0;
+$(document).on('submit',(e)=>{
+    e.preventDefault();
+    let todo:string=$("form #todoInput").val();
+    if(todo)
+    {
+        let todoLI:string=`<li id='${++id}'> ${todo} <span style='color:blue;cursor:pointer' class='delete'> delete</span></li>`;
+        $('.ToDoList').prepend(todoLI);
+        let form=$("#todoForm")[0] as any;
+        form.reset();
+    }
+});
+$(document).ready(()=>{
+    $('.ToDoList').on('click',(e)=>
+        {
+        let LiElement=e.target.parentNode as HTMLElement; 
+        LiElement.remove();   
+        }
+    );
+})
